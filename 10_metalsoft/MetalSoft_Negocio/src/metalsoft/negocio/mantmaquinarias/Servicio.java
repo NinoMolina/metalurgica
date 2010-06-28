22982fc1c2a33b8b5a503203cc7adda39f26b7b7
nino32 <NinoMolina@users.noreply.github.com>
2010-06-28 01:59:54 +0000

    metodos update en algunas clases no estan implementados

--- 10_metalsoft/MetalSoft_Negocio/src/metalsoft/negocio/mantmaquinarias/Servicio.java
+++ 10_metalsoft/MetalSoft_Negocio/src/metalsoft/negocio/mantmaquinarias/Servicio.java
@@ -1,6 +1,6 @@
 //Source file: D:\\Mis documentos\\Facultad\\Proyecto Final\\Repositorio\\metalurgica\\10_metalsoft\\rational\\metalsoft\\sistema\\mantmaquinarias\\Servicio.java
 
-package metalsoft.sistema.mantmaquinarias;
+package metalsoft.negocio.mantmaquinarias;
 
 
 public class Servicio 
