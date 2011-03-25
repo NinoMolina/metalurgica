f989d8273fcb746e25ad5264ae9d91cfc23a6981
nino32 <NinoMolina@users.noreply.github.com>
2011-03-25 22:01:41 +0000

    NuevoUsuario

--- 10_metalsoft/MetalSoft_Desktop/src/metalsoft/datos/jpa/entity/Sesion.java
+++ 10_metalsoft/MetalSoft_Desktop/src/metalsoft/datos/jpa/entity/Sesion.java
@@ -10,11 +10,14 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -36,6 +39,8 @@ public class Sesion implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "session_seq")
+    @SequenceGenerator(name = "session_seq", sequenceName = "sesion_idsesion_seq", allocationSize = 1)
     @Column(name = "idsesion")
     private Long idsesion;
     @Column(name = "fechainicio")
