c73d5700fb70f586c818353e17ea2c99d614d2d1
nino32 <NinoMolina@users.noreply.github.com>
2011-08-14 03:53:20 +0000

    creacion de clase dispacher para que se pueda filtrar el menu segun el rol del usuario y lanzar o no los hilos de escucha de fin de etapa y chequeo de etapa atrasada.

--- 10_metalsoft/MetalSoft_Desktop/src/metalsoft/Main.java
+++ 10_metalsoft/MetalSoft_Desktop/src/metalsoft/Main.java
@@ -2,10 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package metalsoft.presentacion;
+package metalsoft;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import metalsoft.presentacion.AbrirSesion;
 import metalsoft.presentacion.lookandfeel.LookAndFeelManager;
 import metalsoft.presentacion.lookandfeel.Skins;
 
